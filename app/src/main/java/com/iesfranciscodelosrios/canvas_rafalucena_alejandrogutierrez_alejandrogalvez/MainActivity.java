2001a0c1cbd3a7ff485950152fda4e5abdcba735
rafa30052003 <rafalucenavalle9@gmail.com>
2024-01-29 09:29:29 +0100

    first commit

---
+++ app/src/main/java/com/iesfranciscodelosrios/canvas_rafalucena_alejandrogutierrez_alejandrogalvez/MainActivity.java
@@ -0,0 +1,14 @@
+package com.iesfranciscodelosrios.canvas_rafalucena_alejandrogutierrez_alejandrogalvez;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

da3bd5ca19abc07b0d2223dc844dced601518aa3
rafa30052003 <rafalucenavalle9@gmail.com>
2024-02-14 16:36:13 +0100

    button color

--- app/src/main/java/com/iesfranciscodelosrios/canvas_rafalucena_alejandrogutierrez_alejandrogalvez/MainActivity.java
+++ app/src/main/java/com/iesfranciscodelosrios/canvas_rafalucena_alejandrogutierrez_alejandrogalvez/MainActivity.java
@@ -3,12 +3,25 @@ package com.iesfranciscodelosrios.canvas_rafalucena_alejandrogutierrez_alejandro
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private ColorClass miCanvas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        miCanvas = findViewById(R.id.miCanvas);
+
+        findViewById(R.id.botonCambiarColor).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Al hacer clic en el botón, se llama al método cambiarColorLapiz en ColorClass
+                miCanvas.cambiarColorLapiz();
+            }
+        });
     }
+
 }
